38a3eb09103d32a015f59771563ef63322bfed5b
gracebarnett <gracebarnett288@gmail.com>
2023-01-18 14:16:06 -0500

    update 2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,13 @@
 import os
 import csv
+
 #Create path
 election_csv = os.path.join("Resources", "election_data.csv")
+
 #Analysis
 print(f"Election Results\n"
   f"----------------------------")
+
 #Set variables
 total_votes = []
 candidate_list = []
@@ -14,18 +17,30 @@ candidate_votes = {}
 percentage_votes = 0
 #candidate_votes = []
 winner = 0
+
 #Read file
-with open(election_csv) as csvfile:
+with open(election_csv) as csvfile: 
     csv_reader = csv.reader(csvfile)
+
+    #Skip headers
+    header = next(csv_reader)
+
     for row in csv_reader:
+
 #Count total votes
         total_votes.append(row[1])
 print(f'Total Votes: {len(total_votes)}')
+
 #Separate
 print(f"----------------------------")
+
 #List candidates
 with open(election_csv) as csvfile:
     csv_reader = csv.reader(csvfile)
+
+    #Skip headers
+    header = next(csv_reader)
+
     for row in csv_reader:
         candidate_name = row[2]
         if candidate_name not in candidate_list:
@@ -43,3 +58,8 @@ with open(election_csv) as csvfile:
         #print(candidate_name)
     print(candidate_votes)
 
+#Separate
+print(f"----------------------------")
+
+# Sort list
+sorted_list = sorted(candidate_votes)
