f0d7c49c1bda52b68221d46db8be780d5668feca
gracebarnett <gracebarnett288@gmail.com>
2023-01-18 16:32:03 -0500

    update 4

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,19 +3,21 @@ import csv
 
 #Create path
 election_csv = os.path.join("Resources", "election_data.csv")
+analysis_txt = os.path.join("analysis", "election_analysis.txt")
 
 #Analysis
 print(f"Election Results\n"
   f"----------------------------")
 
 #Set variables
-total_votes = []
+total_votes = 0
 candidate_list = []
 unique_candidates = []
 candidate_name = []
 candidate_votes = {}
 percentage_votes = 0
 #candidate_votes = []
+winning_candidate = ""
 winner = 0
 
 #Read file
@@ -28,8 +30,8 @@ with open(election_csv) as csvfile:
     for row in csv_reader:
 
 #Count total votes
-        total_votes.append(row[1])
-print(f'Total Votes: {len(total_votes)}')
+       total_votes = total_votes + 1
+print(f'Total Votes: (len(total_votes))')
 
 #Separate
 print(f"----------------------------")
@@ -47,13 +49,13 @@ with open(election_csv) as csvfile:
             candidate_list.append(candidate_name)
             candidate_votes[candidate_name] = 0
             #candidate_list.append(row[2])
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] 
         for i in set(candidate_list):
             unique_candidates.append(i)
 
  #Total votes per candidate
         y = candidate_list.count(i)
-        candidate_votes.update(range(y))
+        candidate_votes.update({candidate_name: candidate_votes})
         #print(candidate_name)
     print(candidate_votes)
 
@@ -62,8 +64,14 @@ print(f"----------------------------")
 
 #Sort list
 sorted_list = sorted(candidate_votes)
+arrange_list = sorted_list
 
-# Percentage of votes per candidate
+count_candidate = candidate_votes(arrange_list)
+candidate_votes.append(count_candidate.most_common())
 
-z = (y/candidate_votes)*100
-percentage_votes.append(z)
+for item in candidate_votes:
+       
+        first = format((item[0][1])*100/(sum(count_candidate.values())),'.3f')
+        second = format((item[1][1])*100/(sum(count_candidate.values())),'.3f')
+        third = format((item[2][1])*100/(sum(count_candidate.values())),'.3f')
+        fourth = format((item[3][1])*100/(sum(count_candidate.values())),'.3f')
